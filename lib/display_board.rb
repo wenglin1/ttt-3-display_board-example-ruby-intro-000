1af34108985ba54551b11643cef75f0cd2978766
weng lin <wenglin@buffalo.edu>
2019-11-22 19:11:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,10 @@
 def display_board# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+  puts "A Tic Tac Toe Board"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
+
+display_board
